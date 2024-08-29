d3aefc0f9c31135de5b11bc4c180bca4a18d72f7
utuedey <utuedeyejosph@gmail.com>
2024-08-29 23:00:27 +0100

    Add frontend files

---
+++ frontend/src/components/pages/loginPage.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+const LoginPage = () => {
+
+    const [email, setEmail ] = useState();
+  return (
+    <div>loginPage</div>
+  )
+}
+
+export default LoginPage
---
+++ frontend/src/components/pages/signupPage.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import Input from '../Input';
+import { User } from 'lucide-react';
+
+const SignupPage = () => {
+
+    const [name, setName] = useState('');
+
+    const handleSignUp = (e) => {
+        e.preventDefault();
+    }
+  return (
+    <motion.div
+    initial={{ opacity: 0, y: 20}}
+    animate={{ opacity: 1, y: 0}}
+    transition={{ duration: 0.5 }}
+    className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl
+    overflow-hidden'
+    >
+    <div className='p-8'>
+    <h2
+       className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500
+       text-transparent bg-clip-text'>
+        Create Account
+       </h2>
+       <form onSubmit={handleSignUp}>
+       <Input
+       icon={User}
+       type='text'
+       placeholder='Full Name'
+       value={name}
+       onchange={(e) => setName(e.target.value)}
+       />
+       </form>
+    
+    </div>
+    </motion.div>
+  )
+}
+
+export default SignupPage
