e120dc2da72cc6bac19fd44cb08c5237c7f37678
utuedey <utuedeyejosph@gmail.com>
2024-09-08 14:24:39 +0100

    Add cors

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ const { connectToDatabase } = require('./models/db');
 const authRoutes = require('./routes/auth');
 const cookieParser = require("cookie-parser");
 const app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.json()); // allows us to parse incoming requests with JSON payloads
 app.use(cookieParser()); // allows us to parse incoming cookies
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // Routes
 app.use('/api/auth', authRoutes);
