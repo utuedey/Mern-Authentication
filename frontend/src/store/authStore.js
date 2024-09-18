dfe1e6ad735d71d904edd16b4eee6bfbe2aa05ab
utuedey <utuedeyejosph@gmail.com>
2024-09-18 22:58:38 +0100

    Add authStore

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -10,6 +10,7 @@ export const useAuthStore = create((set) => ({
     error: null,
     isLoading: false,
     isCheckingAuth: true,
+    message: null,
 
     signup: async(email, password, username) => {
         set({isLoading: true, error: null});
@@ -75,5 +76,30 @@ export const useAuthStore = create((set) => ({
     } catch (error) {
         set({ error: "Error logging out", isLoading: false })
         throw error;
-    }}
+    }},
+    forgotPassword: async (email) => {
+        set({ isLoading: true, error: null})
+        try {
+            const response = await axios.post(`${API_URL}/forgot-password`, {email})
+            set({ message: response.data.message, isLoading: false})
+        } catch (error) {
+            set({ isLoading: false,
+                error: error.response.data.message || "Error sending reset password email",
+            });
+            throw error;
+        }
+    },
+    resetPassword: async(token, password) => {
+        set({ isLoading: true, error: null})
+        try {
+            const response = await axios.post(`${API_URL}/reset-password/${token}`, {password})
+        set({message: response.data.message, isLoading: false})
+        } catch (error) {
+            set({
+                isLoading: false,
+                error: error.response.data.message || "Error resetting password",
+            });
+            throw error;
+        }
+    }
 }));
